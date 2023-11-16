4e0997b1124d64e207804e04a304e66f71252a59
Jenihacker <20cs064.jenison@sjec.ac.in>
2023-11-16 21:44:35 +0530

    Initial Commit

---
+++ Leaderboard-Frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import LeaderboardCard from './components/LeaderboardCard'
+
+function App() {
+  return (
+    <>
+      <h1 className="text-5xl my-8 font-orbitron font-semibold drop-shadow-[0_0_5px_#7d03bb]">
+        TechWiz Leaderboard
+      </h1>
+      <LeaderboardCard />
+    </>
+  );
+}
+
+export default App
---
+++ Leaderboard-Frontend/src/components/LeaderboardCard.tsx
@@ -0,0 +1,126 @@
+import { Reorder } from "framer-motion";
+import { useEffect, useState } from "react";
+
+let colors: string[] = [
+  "bg-red-500",
+  "bg-orange-500",
+  "bg-amber-500",
+  "bg-pink-500",
+  "bg-green-500",
+  "bg-violet-500",
+  "bg-fuchsia-600",
+  "bg-purple-800",
+  "bg-emerald-500",
+  "bg-rose-500",
+];
+
+interface Teams {
+  TeamNumber: number;
+  Name: string;
+  Points: number;
+  Credits: number;
+}
+
+function LeaderboardCard() {
+  const [data, setdata] = useState([] as any);
+
+  // useEffect(()=>{
+  //   fetchDetails();
+  // },[data]);
+
+  setTimeout(()=>{fetchDetails();},2000);
+
+  const fetchDetails = async () => {
+    const url = "http://127.0.0.1:8000/leaderboard/";
+    const options = {
+      method: "GET",
+    };
+
+    try {
+      const response = await fetch(url, options);
+      const result : Array<Teams> = await response.json();
+      console.log(result);
+      setdata(result.sort((a , b) => b.Points - a.Points));
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  return (
+    <Reorder.Group as="table" className="table-fixed" axis="y" values={data} onReorder={setdata}>
+      <thead>
+        <tr className="flex justify-between mx-5 mr-6">
+          <th className="font-semibold text-xl font-poppins">Rank</th>
+          <th className="font-semibold text-xl font-poppins w-full">Team name</th>
+          <th className="font-semibold text-xl font-poppins mx-10">Credits</th>
+          <th className="font-semibold text-xl font-poppins">Score</th>
+        </tr>
+      </thead>
+      <tbody>
+        {data.map((e: Teams, index: number) => (
+          <Reorder.Item
+            as="tr"
+            value={e}
+            key={e.TeamNumber}
+            className={`${
+              colors[index % 10]
+            } rounded-full flex my-4 text-white`}
+          >
+            <td className="w-24 h-20 flex justify-center items-center">
+              {index === 0 && (
+                <div className="w-12 h-12 mx-5 rounded-[50%] flex justify-center items-center text-2xl font-bold">
+                  <img
+                    className="scale-125"
+                    src="https://png.pngtree.com/png-vector/20220731/ourmid/pngtree-gold-medal-1st-place-award-icon-png-image_6093703.png"
+                    alt="first"
+                  />
+                </div>
+              )}
+              {index === 1 && (
+                <div className="w-12 h-12 mx-5 rounded-[50%] flex justify-center items-center text-2xl font-bold">
+                  <img
+                    className="scale-125"
+                    src="https://png.pngtree.com/png-vector/20220731/ourmid/pngtree-silver-medal-2nd-place-award-icon-png-image_6093704.png"
+                    alt="second"
+                  />
+                </div>
+              )}
+              {index === 2 && (
+                <div className="w-12 h-12 mx-5 rounded-[50%] flex justify-center items-center text-2xl font-bold">
+                  <img
+                    className="scale-125"
+                    src="https://png.pngtree.com/png-vector/20220731/ourmid/pngtree-3rd-place-bronze-medal-award-icon-png-image_6093735.png"
+                    alt="third"
+                  />
+                </div>
+              )}
+              {index != 0 && index != 1 && index != 2 && (
+                <div className="w-12 h-12 mx-5 rounded-[50%] flex justify-center items-center text-2xl font-bold">
+                  {/* <img
+                  className="scale-125 rounded-full"
+                  src={e.thumbnails}
+                  alt="third"
+                /> */}
+                  {index + 1}
+                </div>
+              )}
+            </td>
+            <td className="flex justify-start items-center w-full sm:min-w-[280px] lg:min-w-[280px] lg:pl-16 ">
+              <div className="flex flex-col text-left">
+                <span className="font-bold text-xl">{e.Name}</span>
+                <span className="font-semibold text-lg">{""}</span>
+              </div>
+            </td>
+            <td className="flex items-center mx-10 text-2xl text-white font-bold">
+              {e.Credits}
+            </td>
+            <td className="flex items-center mx-10 text-2xl text-white font-bold">
+              {e.Points}
+            </td>
+          </Reorder.Item>
+        ))}
+      </tbody>
+    </Reorder.Group>
+  );
+}
+
+export default LeaderboardCard;
